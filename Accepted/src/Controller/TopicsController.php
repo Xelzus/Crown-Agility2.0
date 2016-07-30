f6762112dd9d7dbe13ffd70a771a6b8d75b97833
ctalave1 <talavera.christopher@yahoo.com>
2016-07-30 19:52:09 -0400

    Allows a user to see profile pic in forum posts
    Allows a user to see profile pic in forum posts

--- Accepted/src/Controller/TopicsController.php
+++ Accepted/src/Controller/TopicsController.php
@@ -14,6 +14,7 @@ class TopicsController extends AppController
     {
         parent::initialize();
     	$this->loadModel('Posts');
+        $this->loadModel('Profiles');
     }
 
 	public function beforeFilter(Event $event) {
@@ -85,7 +86,7 @@ class TopicsController extends AppController
 
 		 $this->paginate = [ 'Posts' =>
 		 array(
-		 'contain' => array('Users'),
+		 'contain' => array('Users' => ['Profiles']),
 		 'conditions' => array('Posts.topic_id'=>$topic['id']),
 		 'order' => array('Posts.id'=>'DESC'))
 		];
